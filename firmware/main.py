9cb9ac23bbf3d1d695140c1c5637c104d5bdd639
Woo <stephan@waldtmann.de>
2023-02-16 19:47:42 +0100

    Gedöns

--- firmware/main.py
+++ firmware/main.py
@@ -1,11 +1,41 @@
-#
-# this script assumes the default connection of the I2C bus
-# On pycom devuces that is P9 = SDA, P10 = scl
-#
+# I2C Interface, genuine Micropython
+
+from board.bohei import Board
+#from config import Config
+#from display import Display
+from ewh_net import Network
+from heartbeat import Heartbeat
+from name import Name
+#from mqtt import MQTT
+from task import Scheduler
+#import ntptime
+
+
+from bme680i import BME680_I2C
 from machine import I2C, Pin
-import bme280_float as bme280
+import time
+bme = BME680_I2C(I2C(-1, Pin(22), Pin(21)))
 
-i2c = I2C(-1, Pin(22), Pin(21))
-bme = bme280.BME280(i2c=i2c)
 
-print(bme.values)
+network = Network()
+board = Board(network)
+board.init()
+
+heartbeat = Heartbeat(board.display)
+
+
+scheduler = Scheduler()
+
+name = Name(board.display)
+scheduler.register(board.display)
+scheduler.register(heartbeat)
+scheduler.register(network)
+
+print("Starting scheduler!")
+
+scheduler.start(100)
+
+# #for _ in range(3):
+# while True:
+#     print(bme.temperature, bme.humidity, bme.pressure, bme.gas)
+#     time.sleep(1)
